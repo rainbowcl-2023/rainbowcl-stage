828f9ce5855a966e326cee60b6f5182baaffce01
Tiam-Rietsch <NgnotchoueTiam@gmail.com>
2023-09-01 16:11:35 +0100

    menu dropdown done

--- navbar_script.js
+++ navbar_script.js
@@ -25,3 +25,43 @@ languageItems.forEach(item => item.addEventListener('click', (event) => {
   selectedImage.src = newImage.src
   selectedSpan.innerHTML = languages[newSpan.innerHTML]
 }))
+
+// ========================================== Handles the drop down menu button
+
+const menuButton = document.querySelector('#dropdown-button')
+const menuList = document.querySelector('#dropdown-list')
+
+
+function morphToCloseButton() {
+  const spanLines = menuButton.querySelectorAll('.dropdown-span')
+  spanLines.forEach(item => {
+    item.style['position'] = 'absolute'
+  } )
+  spanLines[0].style['rotate'] = '45deg'
+  spanLines[0].style['width'] = '60%'
+  spanLines[1].style['rotate'] = '-45deg'
+  spanLines[1].style['width'] = '60%'
+  spanLines[2].style['rotate'] = '45deg'
+  spanLines[2].style['width'] = '60%'
+
+  menuButton.classList.remove('menu-button')
+}
+
+function morphToMenuButton() {
+  const spanLines = menuButton.querySelectorAll('.dropdown-span')
+  spanLines.forEach(item => {
+    item.style['position'] = 'relative'
+    item.style['top'] = 'unset'
+    item.style['left'] = 'unset'
+    item.style['transform'] = 'unset'
+    item.style['rotate'] = '0deg'
+    item.style['width'] = '100%'
+  } )
+
+  menuButton.classList.add('menu-button')
+}
+
+menuButton.addEventListener('click', (event) => {
+  event.target.classList.contains('menu-button') ? morphToCloseButton() : morphToMenuButton()
+  menuList.classList.toggle('visible-menu')
+})
