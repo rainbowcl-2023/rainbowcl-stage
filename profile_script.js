9702cd7c7b6ee776cc7fadb2d22755e34bd541ae
Tiam Rietsch <NgnotchoueTiam@gmail.com>
2023-09-05 16:12:36 +0100

    added close button and form

--- profile_script.js
+++ profile_script.js
@@ -4,7 +4,16 @@ editProfileButton.addEventListener('click', (event) => {
   const editSection = document.querySelector('#edit-profile-section')
   const body = document.querySelector('body')
   editFilter.classList.remove('none-profile-filter')
-  editSection.style['right'] = '100%'
   editSection.style['animation'] = 'swipe-edit-section 0.4s ease forwards'
-  body.style['overflow'] = 'hidden'
+  body.style['overflow-y'] = 'hidden'
+})
+
+const closeEditButton = document.querySelector('#close-edit-btn')
+closeEditButton.addEventListener('click', (event) => {
+  const editFilter = document.querySelector('#edit-profile-filter')
+  const editSection = document.querySelector('#edit-profile-section')
+  const body = document.querySelector('body')
+  editSection.style['animation'] = 'close-edit-section 0.4s ease forwards'
+  setTimeout(() => editFilter.classList.add('none-profile-filter'), 0.5 * 1000)
+  body.style['overflow-y'] = 'scroll' 
 })
